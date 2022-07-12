bea080026caacc56d19098899217be14a383463b
Arleta <aszamer@wp.pl>
2022-07-12 22:43:00 +0100

    Kalkulator dla integer z parseInt (konwersja String na Int)

--- src/main/java/pl/cyber/trainees/Calculator.java
+++ src/main/java/pl/cyber/trainees/Calculator.java
@@ -8,5 +8,9 @@ public class Calculator {
         new Multiply(25,10);
         new Divide (28,0);
         new Divide (28,4);
+       // String x1="15";
+       // String x2="3";
+       // int x3=Integer.parseInt(x1);
+       // System.out.println(x3);
     }
 }
