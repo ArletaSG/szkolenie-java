074c5c66cff26dd5903b3e726d8913f8c078af28
Arleta <aszamer@wp.pl>
2022-07-15 17:06:09 +0100

    utworzenie klas z get i set z wpisaniem nowego pojazdu i wyświetleniu

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -13,14 +13,23 @@ public class SimpleApp {
         var modelPojazdu3 = new Model("Toyota", "2020",
         "4x4", "Rav4", 10000,
         2.0, Kolor.valueOf("CZERWONY"));
+        modelPojazdu1.setKolor(Kolor.CZERWONY);
+        modelPojazdu1.setNazwa("Fiesta");
+        modelPojazdu1.setPrzebieg(25000);
+        modelPojazdu1.setPojemnoscSilnika(1.4);
+        modelPojazdu1.setMarka("Ford");
+        modelPojazdu1.setNaped("przód");
+        modelPojazdu1.setRocznik("2015");
 
         System.out.println(modelPojazdu1.toString());
+
         System.out.println(modelPojazdu2.toString());
         System.out.println(modelPojazdu3.toString());
         System.out.println(modelPojazdu2.getMarka());
         System.out.println(modelPojazdu3.getNaped());
         System.out.println(modelPojazdu3.getNazwa());
     }
+
 
 
 }
