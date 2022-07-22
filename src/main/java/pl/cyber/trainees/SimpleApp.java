e07b1a770e2d523107bacbed7aa7b5aaae0f1a69
Arleta <aszamer@wp.pl>
2022-07-22 19:06:25 +0100

    opis struktury klas i metod

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,7 +1,18 @@
-package pl.cyber.trainees;
+//1. Konstrukcja klas i metod
 
-import pl.cyber.trainees.dziedziczenie.Kolor;
-import pl.cyber.trainees.dziedziczenie.Model;
+package pl.cyber.trainees; //określenie pakietu w ramach projektu, gdzie znajduje się nasza klasa
+
+// pole package ustawia się automatycznie jeśli nasz plik zawiera się w odpowiednim strukturze
+//katalogów tj. src/main/java/...
+//w naszym przykładzie src/main/java/pl.cyber.trainees   TO jest potrzebne do modyfikataorów dostępu (public, private/protected i difault)
+
+//region miejsca ba importy
+//tu można wrzucać i zwijać
+import java.io.File; //importy zazwyczaj dodają się same, ewent. można je dodać poprzez CTRL+Enter
+                     // poprzez importy powinniśmy rozumieć użycie dodatkowych bibliotek w naszej klasie
+import java.math.*;  //  z * mówimy, że chcemy zaimportowac wszystkie biblioteki w ramach pakietu (package) w tym java.math
+                     // jak usunuemy * to można zaiportować BigDecimal RoundingMode i inne, z * wszystkie
+//endregion
 
 public class SimpleApp {
     public static void main(String[] args) {
